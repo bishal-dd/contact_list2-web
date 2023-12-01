87a1262679bb6f5569dc64932a09b243e571b72f
bishal-dd <dhakalbishal930@gmail.com>
2023-12-01 21:22:36 +0600

    hygen setup

--- scripts/newQueryMock.ts
+++ scripts/newQueryMock.ts
@@ -1,10 +1,10 @@
 import { hideBin } from "yargs/helpers";
 import yargs from "yargs/yargs";
-import { spawnSync } from "./util/spawnSync";
+import { spawnSync } from "./util/spawnSync.ts";
 
 const argv = yargs(hideBin(process.argv)).argv as unknown as { file: string };
 
-spawnSync("yarn tsc:scripts");
+spawnSync("npm run tsc:scripts");
 spawnSync(
   argv.file
     ? `node scripts/graphql/createMock.js --file ${argv.file}`
